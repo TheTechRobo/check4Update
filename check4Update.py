403a069556499afc38b06fb7f9b0f5c7d7db9f37
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2019-12-11 18:53:46 -0500

    Update check4Update.py
    A lot

--- check4Update.py
+++ check4Update.py
@@ -1,11 +1,24 @@
-v = "1.0"
-from urllib.request import urlopen
+from subprocess import Popen #To launch update script
+from urllib.request import urlopen #To read URL
+v = "1.0" #Variable of using version
 
-link = "https://thetechrobo.github.io/checkVersion"
-f = urlopen(link)
-nV = f.read()
-print("The new version is ", nV)
+link = "https://thetechrobo.github.io/checkVersion" #URL to download
+try:
+    f = urlopen(link) #Download URL
+    nV = f.read() #Read the downloaded URL
+except: #If error occurs
+    print("An error occured in downloading website..")
+    unsu = True
+finally: #whether error occurs or not
+    if unsu == True
+        print("Process failed")
+    else:
+        print("Process success!")
 if nV != v:
+    print("Latest version:", nV)
     print("You are utilising version ", v)
-elif nv == v:
-    print("You have the Latest Version")
+    yN = input("Download newest version?? y/n, case-sensitive")
+    if yN == "y":
+        
+elif nV == v:
+    print("You have the Latest Version ;)")

b19dc9f7316dcfed396d3609f8001a2b92128ca8
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2019-12-11 19:15:58 -0500

    Create u.py

---
+++ u.py
@@ -0,0 +1,24 @@
+def u():
+    from sys import exit
+    class Cancelled(Exception):
+        print("Cancelled")
+    exit("CANCELLED PROCESS -- The script was interrupted")
+    print("This will OVERWRITE all files with same name in current working dir!!")
+    cont = input("Would you still like to continue? y/n, case-sensitive")
+    if cont == "y":
+        print("removing files..")
+        os.system("rm -f FileName")
+        print("33% done")
+        os.system("rm -f FileName")
+        print("66% done")
+        os.system("rm -f FileName")
+        print("100% done, finished deletion")
+        print("Downloading update")
+        os.system("curl -o FileName https://link.to/file1")
+        print("33% done")
+        os.system("curl -o FileName https://link.to/file2")
+        print("66% done")
+        os.system("curl -o FileName https://link.to/file3")
+        print("100% done, finished download")
+    else:
+        raise Cancelled

ca2fe4e1c01307da925da183841f123f7442b6d7
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-03-28 08:24:45 -0400

    Update u.py
    Added responsive to keyboardinterrupt

--- u.py
+++ u.py
@@ -5,19 +5,22 @@ def u():
     print("This will OVERWRITE all files with same name in current working dir!!")
     cont = input("Would you still like to continue? y/n, case-sensitive")
     if cont == "y":
-        print("removing files..")
-        os.system("rm -f FileName")
-        print("33% done")
-        os.system("rm -f FileName")
-        print("66% done")
-        os.system("rm -f FileName")
-        print("100% done, finished deletion")
-        print("Downloading update")
-        os.system("curl -o FileName https://link.to/file1")
-        print("33% done")
-        os.system("curl -o FileName https://link.to/file2")
-        print("66% done")
-        os.system("curl -o FileName https://link.to/file3")
-        print("100% done, finished download")
+        try:
+            print("removing files..")
+            os.system("rm -f FileName")
+            print("33% done")
+            os.system("rm -f FileName")
+            print("66% done")
+            os.system("rm -f FileName")
+            print("100% done, finished deletion")
+            print("Downloading update")
+            os.system("curl -o FileName https://link.to/file1")
+            print("33% done")
+            os.system("curl -o FileName https://link.to/file2")
+            print("66% done")
+            os.system("curl -o FileName https://link.to/file3")
+            print("100% done, finished download")
+        except KeyboardInterrupt:
+            raise Cancelled
     else:
         raise Cancelled

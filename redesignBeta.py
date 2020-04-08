81553d0329daca63b43904cd444c52f447a96c71
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-04-08 14:34:53 -0400

    Add changelog

--- redesignBeta.py
+++ redesignBeta.py
@@ -7,9 +7,13 @@ def download(filename, url):
     file_ = open(filename, 'w')
     file_.write(data)
     file_.close()
+def view(url):
+    response = urlopen(url)
+    data = response.read()
+    #output
 version = 1.0 #update this every time, or else it will not work properly. It has to be CORRECT!
 link = "https://thetechrobo.github.io/checkVersion" #link to file with version of program
-newVer = urlopen(url)
+newVer = view(url)
 newVer = newVer.read()
 print("Latest version: ", newVer)
 print("Current version: ", version)
@@ -17,7 +21,13 @@ if newVer == version:
     print("You have the latest version.")
 else:
     print("There is a new version available!")
-    want = input("Do you want to download it? (Y/n) ")
+    see = input("Do you want to view the changelog? Y/n: ")
+    see.lower()
+    if see == "y":
+        view(link.to/changelog)
+    else:
+        print("OK, skipping...")
+    want = input("Do you want to download the new version? (Y/n) ")
     want = want.lower()
     if want == "y":
         print("OK, downloading it")
@@ -27,4 +37,4 @@ else:
         download(thirdfilename.py, url.to/file3)
         print("Success !")
         print("Now starting the updated version...")
-        #do runpy
+        #start your program here!

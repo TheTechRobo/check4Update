4313bd4d9f0d875b37477bc3b4d9e9d7f1161992
Thittussarom <ittussarom@TtRTech-PC>
2020-04-19 18:59:43 -0400

    Delete old

--- check4UpdateBeta.py
+++
@@ -1,7 +0,0 @@
-import requests
-def down(url, filename):
-    downloadedFile = requests.get(url, stream=True)
-    downloadedFile = downloadedFile.get()
-    with open(filename, "wb") as handle:
-        handle.write(response)
-down("https://hello.ca", "hello.html")

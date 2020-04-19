6a5b5289ddbeb2e8ff4289805de0ae5930281407
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-04-19 19:30:46 -0400

    Update check4Update.py

--- check4Update.py
+++ check4Update.py
@@ -9,8 +9,31 @@ def download_file(url):
     with requests.get(url, stream=True) as r:
         with open(local_filename, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
-
-    return local_filename
+            return local_filename
+def view(url):
+    viewer = requests.get(url)
+    viewer = viewer.raw()
+    return viewer
+print("Checking for updates...")
+Version = open("version", "r")
+onlineVersion = view("https://thetechrobo.github.io/assets/version")
+if Version == onlineVersion:
+    print("You are already up-to-date.")
+else:
+    print("You have version ", Version, "! But...there is version ", onlineVersion, " available.")
+    download = input("Would you like to download it y/n? ")
+    download = download.lower()
+    if download == "n":
+        exit("Abort.")
+    else:
+        print("OK, continuing...")
+changelog = input("Would you like to view the changelog? Y/n: ")
+changelog = changelog.lower()
+if changelog == "y":
+    changelog = view("https://thetechrobo.github.io")
+    print(changelog.raw())
+else:
+    print("OK. skipping...")
 print("Commencing download...")
 download_file("https://hello.ca")
 print("33% done.", end="\r")

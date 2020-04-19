206e07f7508a51a80f5affd433cf272a527aad0f
Thittussarom <ittussarom@TtRTech-PC>
2020-04-19 19:06:58 -0400

    better

--- check4UpdateOmega.py
+++ check4UpdateOmega.py
@@ -1,5 +1,4 @@
-import requests
-import shutil
+import requests, sys, shutil
 
 def download_file(url):
     local_filename = url.split('/')[-1]
@@ -8,5 +7,10 @@ def download_file(url):
             shutil.copyfileobj(r.raw, f)
 
     return local_filename
+print("Commencing download...")
 download_file("https://hello.ca")
-
+print("33% done.", end="\r")
+download_file("https://thetechrobo.github.io")
+print("66% done.", end="\r")
+download_file("http://thetechrobo.byethost8.com")
+print("Done download!", end="\r")
